221d99c6f07366902e882bf00751f37c9fc340eb
canonphp <pepejaja9055@gmail.com>
2024-11-25 00:53:38 +0100

    theme

--- src/PepeTheme.php
+++ src/PepeTheme.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Jsanchezfactoriabiz\Pepe;
 
 use Filament\Contracts\Plugin;
 use Filament\Panel;
 use Filament\Support\Assets\Theme;
-use Filament\Support\Color;
+use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentAsset;
 
-class Skeleton implements Plugin
+class Pepe implements Plugin
 {
     public function getId(): string
     {
-        return 'skeleton';
+        return 'pepe';
     }
 
     public function register(Panel $panel): void
     {
         FilamentAsset::register([
-            Theme::make('skeleton', __DIR__ . '/../resources/dist/skeleton.css'),
+            Theme::make('pepe', __DIR__ . '/../resources/dist/pepe.css'),
         ]);
 
         $panel
@@ -29,7 +29,7 @@ class Skeleton implements Plugin
             ->dangerColor(Color::Rose)
             ->successColor(Color::Green)
             ->grayColor(Color::Gray)
-            ->theme('skeleton');
+            ->theme('pepe');
     }
 
     public function boot(Panel $panel): void
