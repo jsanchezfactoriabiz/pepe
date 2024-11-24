bd2fdbfa91398c702ee195effc4ccdec3f437b87
jsanchezfactoriabiz <jsanchezfactoriabiz@users.noreply.github.com>
2024-11-24 23:54:02 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,10 +13,10 @@ use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Jsanchezfactoriabiz\Pepe\PepeServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Jsanchezfactoriabiz\Pepe\PepeServiceProvider;
 
 class TestCase extends Orchestra
 {
